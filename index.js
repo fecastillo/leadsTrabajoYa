98b54c6c1c7a0abba39c09ae4f0567de852492a8
fernando castillo <ferchufer10@gmail.com>
2023-07-26 01:34:49 -0300

    new token method

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const uri = process.env.MONGO_URI;
+let token;
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 // Enter the Page Access Token from the previous step
 const FACEBOOK_PAGE_ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
 // Accept JSON POST body
@@ -32,8 +44,7 @@ app.post('/webhook', async (req, res) => {
     for (const entry of req.body.entry) {
         for (const change of entry.changes) {
             // Process new lead (leadgen_id)
-           // await processNewLead(change.value.leadgen_id);
-            await getToken(change.value.leadgen_id);
+           await processNewLead(change.value.leadgen_id);
         }
     }
 
@@ -41,26 +52,50 @@ app.post('/webhook', async (req, res) => {
     res.send({ success: true });
 })
 
+app.get('/token', async (req, res) => {
+    await refreshAccessCode();
+    res.send({ success: true });
+});
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 });
-//get tokenZoho
-async function getToken(idLead){
-    const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-    const CLIENT_ID = process.env.CLIENT_ID;
-    const CLIENT_SECRET = process.env.CLIENT_SECRET;
+async function getAccessToken(){
+    //descargar refresh token de mongoDB
+    await client.connect();
+    const collection = client.db("creator").collection("autocredito");
+    const result = await collection
+    .find()
+    .sort({
+      _id: -1,
+    })
+    .limit(1)
+    .toArray();
+    const access_token = result[0].access_token;
+    await client.close();
+    return access_token;
+}
+async function refreshAccessCode(){
+    let data = new Object();
     try {
-        const responseTkn = await axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${REFRESH_TOKEN}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=refresh_token`);
-        token = responseTkn.data.access_token;
-        console.log(token);
-        console.log(idLead);
-        await processNewLead(idLead, token);
+        const response = await axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${REFRESH_TOKEN}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=refresh_token`);
+        data.access_token = response.data.access_token;
+        data.timestamp = new Date().toLocaleDateString('es-AR');
+        //subir a mongoDB
+        await uploadMongo(data);
     } catch (error) {
         console.log(error);
     }
 }
+async function uploadMongo(data){
+    await client.connect();
+    await client.db("creator").collection("autocredito").insertOne(data);
+    await client.close();
+}
 //cargar lead en zoho
-async function postZoho(data, tkn){
+async function postZoho(data){
+    //descargar access token de mongoDB
+    token = await getAccessToken();
     var payLoad = new Object();
     payLoad.data = new Object();
     payLoad.data.Fecha_ingreso_dato = new Date().toLocaleDateString('es-AR');
@@ -75,7 +110,7 @@ async function postZoho(data, tkn){
         method: 'post',
         url: 'https://creator.zoho.com/api/v2/autocredito/recursos-humanos-autocredito/form/Datos_a_llamar',
         headers: { 
-          'Authorization': `Zoho-oauthtoken ${tkn}`,
+          'Authorization': `Zoho-oauthtoken ${token}`,
           'Content-Type': 'application/json'
       },
       data : payLoad
@@ -90,7 +125,7 @@ async function postZoho(data, tkn){
       });
 }
 // Process incoming leads
-async function processNewLead(leadId, tknZoho) {
+async function processNewLead(leadId) {
     let response;
     var obj = new Object();
      try {
